5af524155d14ea09dfb4fec1033f646407ab51e8
withsion6928 <withsion6928@gmail.com>
2021-06-20 10:56:44 +0900

    20210620(python) 팩토리얼_재귀함수

---
+++ algo/Solution_10871.py
@@ -0,0 +1,17 @@
+# 팩토리얼 _ 재귀함수 이용
+
+
+var = int(input())
+
+def factori(n):
+    if n == 0:
+        return 1
+
+    if n > 0:
+        return factori(int(n - 1)) * int(n)
+
+
+print(factori(var))
+
+
+

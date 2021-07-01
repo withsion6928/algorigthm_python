be4e82d77cb3eeb9e4a5ceb67eaeae62c1d526a3
withsion6928 <withsion6928@gmail.com>
2021-07-01 19:59:10 +0900

    20210701(python) # 단어공부 https://www.acmicpc.net/problem/1157 complete

---
+++ algo/Solution_1157.py
@@ -0,0 +1,21 @@
+# 단어공부 https://www.acmicpc.net/problem/1157 complete
+word = input()
+
+word = word.upper()
+
+wordList = list(set(word))
+cnt = []
+
+for i in wordList:
+    count = word.count(i)
+    cnt.append(count)
+
+if cnt.count(max(cnt)) >= 2:
+    print("?")
+else:
+    print(wordList[cnt.index(max(cnt))])
+
+
+
+
+
